585ee2002fe46070504bfa8f81c2c18007b12b13
Anastasia <www.nastya97@yandex.ru>
2023-03-22 23:23:03 +0300

    feat: delete link and add dot

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
   const navigate = useNavigate();
